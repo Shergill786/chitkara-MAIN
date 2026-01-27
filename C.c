4c58ab5b4a6c436aa7016afd310764d5b3d43f6c
Shergill786 <jaskirat3426.beai25@chitkara.edu.in>
2026-01-27 19:48:10 +0530

    Add files via upload

---
+++ C.c
@@ -0,0 +1,109 @@
+// #include <stdio.h>
+// int main() {
+//     printf("Hello \nWorld");
+//     return 0;
+// }
+
+// user se input lena hai aur usko print karna hai
+
+
+// #include <stdio.h>
+// int main() {
+//     int num;
+//     printf("Enter your number: ");
+//     scanf("%d", &num);
+//     printf("You entered number: %d", num);
+// }
+
+
+
+// make a calculater
+
+// #include <stdio.h>
+// int main() {
+
+//     float a, b;
+//     float sum, sub, mul, div;
+//     printf("Enter first number: ");
+//     scanf("%f", &a);
+//     printf("Enter second number: ");
+//     scanf("%f", &b);
+//     sum = a + b;
+//     sub = a - b;
+//     mul = a * b;
+//     div = a / b;
+//     printf("Sum: %f\n", sum);
+//     printf("Subtraction: %f\n", sub);
+//     printf("Multiplication: %f\n", mul);
+//     printf("Division: %f\n", div);
+//     return 0;
+//     if (div == 0) {
+//         printf("Division by zero is not allowed.\n");
+//     } 
+//     else {
+//         printf("Division: %f\n", div);
+//     }
+// }
+
+// temp in celsius to fahrenheit
+// #include <stdio.h>
+// int main() {
+//     float celsius, fahrenheit;
+//     printf("Enter temperature in Celsius: ");
+//     scanf("%f", &celsius);
+//     fahrenheit = (celsius * 9/5) + 32;
+//     printf("Temperature in Fahrenheit: %f\n", fahrenheit);
+//     return 0;
+// }
+
+// if -else statement age check
+// #include <stdio.h>
+// int main() {
+//     int age;
+//     printf("Enter your age: ");
+//     scanf("%d", &age);
+//     if (age >= 18) {
+//         printf("You are adult.\n");
+//     } else {
+//         printf("You are not adult.\n");
+//     }
+//     return 0;
+// }
+
+//  if else calculator
+// #include <stdio.h>
+// int main() {
+//     int choice;
+//     float a, b, result;
+//     printf("Select operation:\n");
+//     printf("1. Addition\n");
+//     printf("2. Subtraction\n");
+//     printf("3. Multiplication\n");
+//     printf("4. Division\n");
+//     printf("Enter your choice (1-4): ");
+//     scanf("%d", &choice);
+//     printf("Enter first number: ");
+//     scanf("%f", &a);
+//     printf("Enter second number: ");
+//     scanf("%f", &b);
+//     if (choice == 1) {
+//         result = a + b;
+//         printf("Result: %f\n", result);
+//     } else if (choice == 2) {
+//         result = a - b;
+//         printf("Result: %f\n", result);
+//     } else if (choice == 3) {
+//         result = a * b;
+//         printf("Result: %f\n", result);
+//     } else if (choice == 4) {
+//         if (b != 0) {
+//             result = a / b;
+//             printf("Result: %f\n", result);
+//         } else {
+//             printf("Error: Division by zero is not allowed.\n");
+//         }
+//     } else {
+//         printf("Invalid choice.\n");
+//     }
+//     return 0;
+// }
---
+++ javascript.js
@@ -0,0 +1,20 @@
+let age = 21;
+let name = "Ishaan";
+let isStudent = true;
+let result;
+let data = null;
+let bigNumber = 9007199254740991n;
+let userId = Symbol("1");
+let user = {
+    age: age,
+    name: name,
+    isStudent: isStudent,
+    result: result,
+    data: data,
+    bigNumber: bigNumber,
+    id: userId
+};
+
+console.log(user);
+console.log("Symbol value:", userId);
+console.log("Type of Symbol:", userId);
