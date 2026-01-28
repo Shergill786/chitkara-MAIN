f4677cdd921df851e8de6938ab1fe3f2a89cf25f
Shergill786 <jaskirat3426.beai25@chitkara.edu.in>
2026-01-28 14:45:21 +0530

    modified:   C.c 	new file:   chitjs.js 	modified:   javascript.html 	modified:   javascript.js

---
+++ chitjs.js
@@ -0,0 +1,13 @@
+// let a = Number(prompt("Enter first number: "));
+// let b = Number(prompt("Enter second number: "));
+// console.log("The sum is:", a + b);
+// console.log("The difference is:", a - b);
+// console.log("The product is:", a * b);
+// console.log("The quotient is:", a / b);
+// console.log("The remainder is:", a % b);
+// console.log("The power is:", a ** b);
+
+
+// const age=Number(prompt("Enter your age: "));
+// const mess=(age>=18) ? "You are an adult." : "You are a minor.";
+// console.log(mess);
--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
-console.log("Hello, World!");
-console.log("This is a sample JavaScript file.");
+// console.log("Hello, World!");
+// console.log("This is a sample JavaScript file.");
 
-age=24;
-console.log("Age:", age);
-age+=1;
-console.log("Next Year Age:", age);
-name="JAVASCRIPT";
-console.log("Name:", name);
+// age=24;
+// console.log("Age:", age);
+// age+=1;
+// console.log("Next Year Age:", age);
+// name="JAVASCRIPT";
+// console.log("Name:", name);
 
-x=null;
-y=undefined;
-console.log(x);
-console.log(y);
+// x=null;
+// y=undefined;
+// console.log(x);
+// console.log(y);
 
-isActive=true;
-console.log( isActive);
+// isActive=true;
+// console.log( isActive);
