3b343f72d32cf1013fec3dab67bc80b822daa83f
Shergill786 <jaskirat3426.beai25@chitkara.edu.in>
2026-01-29 09:11:21 +0530

    modified:   tempCodeRunnerFile.js

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+console.log("This is a sample JavaScript file.");
+
+age=24;
+console.log("Age:", age);
+age+=1;
+console.log("Next Year Age:", age);
+name="JAVASCRIPT";
+console.log("Name:", name);
