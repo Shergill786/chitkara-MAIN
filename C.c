ce7e407cdcacd387829096a52b98a4b9e5f4cf89
Shergill786 <jaskirat3426.beai25@chitkara.edu.in>
2026-02-01 19:44:12 +0530

    modified:   C.c 	new file:   C.exe

--- C.c
+++ C.c
@@ -444,3 +444,15 @@
 // }
 
 
+// ternary operator 
+#include <stdio.h>
+int main() {
+    int a, b, max;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    max = (a > b) ? a : b;
+    printf("The maximum number is: %d\n", max);
+    return 0;
+}
+
+
