ec2b2ac01e00cdab8040dccd2f462ca442bb4174
Shergill786 <jaskirat3426.beai25@chitkara.edu.in>
2026-02-01 20:04:33 +0530

    modified:   javascript.js

--- javascript.js
+++ javascript.js
@@ -16,3 +16,27 @@
 // isActive=true;
 // console.log( isActive);
 
+
+// const pi=3.14;
+// console.log("Value of pi:", pi);
+
+// let score=100;
+// score+=50;
+// console.log("Total Score:", score);
+
+
+
+let attempts = 3;
+while (attempts > 0) {
+    let email = prompt("Enter your email address:");
+    switch (true) {
+        case email.includes("@") && email.includes("."):
+            console.log("Valid email address");
+            attempts = 0; 
+            break;
+        default:
+            console.log("Invalid email address");
+            attempts--;
+    }
+}
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,13 @@
-isActive=true;
-co
+let attempts = 3;
+while (attempts > 0) {
+    let email = prompt("Enter your email address:");
+    switch (true) {
+        case email.includes("@") && email.includes("."):
+            console.log("Valid email address");
+            attempts = 0; 
+            break;
+        default:
+            console.log("Invalid email address");
+            attempts--;
+    }
+}
