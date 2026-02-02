6b87a7489ddffaf02a1a34deb8dafcce1bd5d2a0
Shergill786 <jaskirat3426.beai25@chitkara.edu.in>
2026-02-02 21:40:41 +0530

    deleted:    C.exe 	deleted:    tempCodeRunnerFile.c 	deleted:    tempCodeRunnerFile.exe 	deleted:    tempCodeRunnerFile.js

--- tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-
-#include <stdio.h>
-int main() {
-    int age;
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    (age >= 18) ? printf("You are an adult.\n") : printf("You are not an adult.\n");
-    return 0;
-}
