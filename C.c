b4b83c8282ed36f83d637379babbcb707e5e6877
Shergill786 <jaskirat3426.beai25@chitkara.edu.in>
2026-02-03 22:06:50 +0530

    modified:   C.c 	modified:   javascript.js

--- C.c
+++ C.c
@@ -590,3 +590,15 @@
 // | Conditional    | `?:`                               | Right → Left      |              |              |
 // | Assignment     | `=  +=  -=  *=  /=  %=`            | Right → Left      |              |              |
 // | Comma          | `,`                                | Left → Right      |              |              |
+
+
+
+
+// #include <stdio.h>
+// int main() {
+//     int a = 5, b = 10, c = 15;
+//     int result = a + b * c;
+//     printf("Result: %d\n", result);
+//  }
+
+
