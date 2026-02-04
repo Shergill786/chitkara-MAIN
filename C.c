851373a01efab695d4e9ef86c9564c9b6145ebc4
Shergill786 <jaskirat3426.beai25@chitkara.edu.in>
2026-02-04 10:07:03 +0530

    modified:   C.c

--- C.c
+++ C.c
@@ -620,31 +620,33 @@
 //  }
 
 // switch case in grading system
-#include <stdio.h>
-int main() {
-    int x;
-    printf("Enter your marks : ");
-    scanf("%d", &x);
-    switch (x/10) {
-        case 10:
-            printf("Grade A+\n");
-            break;
-        case 9:
-            printf("Grade A\n");
-            break;
-        case 8:
-            printf("Grade B\n");
-            break;
-        case 7:
-            printf("Grade C\n");
-            break;
-        case 6:
-            printf("Grade D\n");
-            break;
-        case 5:
-            printf("Grade E\n");
-            break;
-        default:
-            printf("Grade F\n");
-    }
-}
+// #include <stdio.h>
+// int main() {
+//     int x;
+//     printf("Enter your marks : ");
+//     scanf("%d", &x);
+//     switch (x/10) {
+//         case 10:
+//             printf("Grade A+\n");
+//             break;
+//         case 9:
+//             printf("Grade A\n");
+//             break;
+//         case 8:
+//             printf("Grade B\n");
+//             break;
+//         case 7:
+//             printf("Grade C\n");
+//             break;
+//         case 6:
+//             printf("Grade D\n");
+//             break;
+//         case 5:
+//             printf("Grade E\n");
+//             break;
+//         default:
+//             printf("Grade F\n");
+//     }
+// }
+
+
