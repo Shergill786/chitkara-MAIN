f3e1c437341cd6af017f05ef2a1e2957cb0af1a2
Shergill786 <jaskirat3426.beai25@chitkara.edu.in>
2026-02-11 10:32:18 +0530

    modified:   C.c modified:   C.exe

--- C.c
+++ C.c
@@ -1131,22 +1131,22 @@
 //   12
 //  123
 // 1234
-#include <stdio.h>
-int main() {
-    int n;
-    printf("Enter the number of rows: ");
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n - i; j++) {
-            printf(" ");
-        }
-        for (int k = 1; k <= i; k++) {
-            printf("%d", k);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+// #include <stdio.h>
+// int main() {
+//     int n;
+//     printf("Enter the number of rows: ");
+//     scanf("%d", &n);
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= n - i; j++) {
+//             printf(" ");
+//         }
+//         for (int k = 1; k <= i; k++) {
+//             printf("%d", k);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
 
 
 //    1 
@@ -1164,6 +1164,29 @@ int main() {
 //         }
 //         for (int k = 1; k <= i; k++) {
 //             printf("%d ", k);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+//    1
+//   35
+//  7911
+// 131517
+// #include <stdio.h>
+// int main() {
+//     int n, num = 1;
+//     printf("Enter the number of rows: ");
+//     scanf("%d", &n);
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= n - i; j++) {
+//             printf(" ");
+//         }
+//         for (int k = 1; k <= i; k++) {
+//             printf("%d", num);
+//             num += 2;
 //         }
 //         printf("\n");
 //     }
