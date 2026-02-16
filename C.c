76a90d7157ee37bfe7a29aa438e168914e0f2d5a
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-02-16 09:26:01 +0530

    modified:   C.c 	modified:   C.exe

--- C.c
+++ C.c
@@ -1366,3 +1366,19 @@
 //     printf("\nProgram finished");
 //     return 0;
 // }
+
+// arrays 
+// #include <stdio.h>
+// int main() {
+//     int arr[5];
+//     printf("Enter 5 integers: ");
+//     for (int i = 0; i < 5; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+//     printf("You entered: ");
+//     for (int i = 0; i < 5; i++) {
+//         printf("%d ", arr[i]);
+//     }
+//     return 0;
+// }
+
