06040e2777590f7680ceb88ad2e31023eb560dee
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-02-17 12:36:27 +0530

    modified:   C.c

--- C.c
+++ C.c
@@ -1645,3 +1645,46 @@
 //     }
 //     return 0;
 // }
+
+
+// sum and sub of 2 matrices if they are of same order
+// #include <stdio.h>
+// int main() {
+//     int m, n;
+//     printf("Enter the number of rows and columns: ");
+//     scanf("%d %d", &m, &n);
+//     int m1[m][n], m2[m][n], sum[m][n], diff[m][n];
+//     printf("Enter elements of the first matrix:\n");
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             scanf("%d", &m1[i][j]);
+//         }
+//     }
+//     printf("Enter elements of the second matrix:\n");
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             scanf("%d", &m2[i][j]);
+//         }
+//     }
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             sum[i][j] = m1[i][j] + m2[i][j];
+//             diff[i][j] = m1[i][j] - m2[i][j];
+//         }
+//     }
+//     printf("Sum of the two matrices:\n");
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             printf("%d ", sum[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     printf("Difference of the two matrices:\n");
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             printf("%d ", diff[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
