c1b88de44716dc012087e61e794123faff5a7565
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-02-23 14:53:19 +0530

    modified:   chitjs.js 	new file:   self-c.c

--- chitjs.js
+++ chitjs.js
@@ -547,16 +547,16 @@
 //     }    console.log(row);
 // }
 
-let n=prompt("Enter a number: ");
-for (let i=1;i<=n;i++){
-    let row="";
-    for (let j=1;j<=n-i;j++){
-        row+="  ";
-    }
-    for (let k=1;k<=2*i-1;k++){
-        row+="* ";
-    }   console.log(row);   
-}
+// let n=prompt("Enter a number: ");
+// for (let i=1;i<=n;i++){
+//     let row="";
+//     for (let j=1;j<=n-i;j++){
+//         row+="  ";
+//     }
+//     for (let k=1;k<=2*i-1;k++){
+//         row+="* ";
+//     }   console.log(row);   
+// }
 
 //diamond
 // let n=5;
---
+++ self-c.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+// int main()
+
+// int n,m,r;
+// int rev;
+// int count=0;
+// scanf("%d %d",&n,&m);
+// for(int i=n;i <= m;i++){
+// int temp=i;
+// rev=0;
+// while(temp>0){
+// r=temp%10;
+// rev=(rev*10)+r;
+// temp=temp/10;
+
+// if(i == rev){
+// count++;
+
+// }
+// printf("%d",count);
+// return 0;
+
+// }
