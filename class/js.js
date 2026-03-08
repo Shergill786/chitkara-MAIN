29a871055c9704aba9bd3073f808e4e48117d4c0
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-03-08 21:18:48 +0530

    modified:   class/javascript.html

---
+++ class/js.js
@@ -0,0 +1,100 @@
+// let a = 10;
+// let b = 5;
+
+// console.log(a + b); // Arithmetic
+// console.log(a - b); // Arithmetic
+// console.log(a * b); // Arithmetic
+// console.log(a / b); // Arithmetic
+// console.log(a % b); // Arithmetic
+// console.log(a < b); // Comparison
+// console.log(a > b); // Comparison
+// console.log(a == b); // Comparison
+// console.log(a != b); // Comparison
+// console.log(a >= b); // Comparison
+// console.log(a <= b); // Comparison
+// console.log(a > 5 || b < 10); // Logical
+// console.log(a > 5 && b < 10); // Logical
+// let result = (a > b) ? "A is greater" : "B is greater";
+// console.log(result);
+
+// let day = 3;
+
+// switch(day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+
+
+
+// strings
+// let str1 = "Hello"; 
+
+// string length
+// console.log(str1.length);
+
+// string indexing
+// console.log(str1[0]); 
+
+// template literals
+// let name = "Alice";
+// let age = 30;
+// let greeting = `Hello, ${name} Your age is ${age}.`;
+// console.log(greeting);
+
+// string methods
+
+// string uppercase
+// str.toUpperCase();
+
+// let str = "Hello World";
+// console.log(str.toUpperCase());
+
+
+// string lowercase
+// str.toLowerCase();
+
+// let str = "Hello World";
+// console.log(str.toLowerCase());
+
+
+// string trim (to remove whitespace)
+// str.trim();
+
+// let str = "   Hello World   ";
+// console.log(str.trim());
+
+
+// string slice (returns a part of the string)
+// str.slice(start, end?);
+
+// let str = "Hello World";
+// console.log(str.slice(0, 5)); // Output: Hello
+// console.log(str.slice(6)); // Output: World
+
+
+// string concatination (joins two strings)
+// str.concat(str1);
+// str + str1;
+// let str1 = "Hello";
+// let str2 = "World";
+// console.log(str1.concat(" ", str2));
+
+
+// string replace 
+// str.replace(searchValue, newValue);
+
+// let str = "Hello World";
+// console.log(str.replace("World", "JavaScript")); // Output: Hello JavaScript
+
+
+// string
