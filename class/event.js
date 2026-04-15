2da799dca1cbeb47741f254af2903de7f30e0894
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-04-15 19:34:37 +0530

    modified:   class/event.js

--- class/event.js
+++ class/event.js
@@ -34,3 +34,70 @@
 // addEventListener method in js is used to attach an event handler to a specific element so that it runs when a specific event occurs. 
 // syntax: element.addEventListener(event, function);   
 
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Event Listener Example</title>
+// </head>
+// <body>
+
+//     <button id="btn">Click Me</button>
+
+//     <script>
+//         document.addEventListener("DOMContentLoaded", function() {
+//             const button = document.getElementById("btn");
+//             button.addEventListener("click", function() {
+//                 alert("Button clicked!");
+//             });
+//         });
+//     </script>
+
+// </body>
+// </html>1
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <button id="btn"> change background</button> 
+//     <script>
+//         const btn = document.getElementById('btn');
+//         btn.addEventListener('click', function() {
+//             document.body.style.backgroundColor = 'blue';
+//         });
+//     </script>
+
+// </body>
+// </html>
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <p id="text">Hello, World!</p>
+//     <button id="btn">Click Me</button>
+//     <script>
+//         const btn = document.getElementById('btn');
+//         const text = document.getElementById('text');
+//         btn.addEventListener('click',function(){
+//             text.style.backgroundColor = 'lightblue';
+//             text.style.color = 'white';
+//             text.style.fontFamily = 'Arial, sans-serif';
+//             text.style.fontSize = '200px';
+
+//         });
+//         </script>
+// </body>
+// </html>
+
+
